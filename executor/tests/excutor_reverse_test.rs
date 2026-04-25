4c92082d43007a5da567d014af496963565ff9b2
xy-kia <2993405609@qq.com>
2026-04-25 21:11:39 +0800

    add test cases for reverse

--- executor/tests/excutor_reverse_test.rs
+++ executor/tests/excutor_reverse_test.rs
@@ -13,4 +13,37 @@ mod reverse_tests{
         let expected_pose = Pose::new(-1, 0, 'E');
         assert_eq!(expected_pose, executor.query());
     }
+
+    #[test]
+    fn should_return_s_given_status_is_reverse_command_is_l_and_facing_is_e(){
+        let orginal_pose = Pose::new(0, 0, 'E');
+        let mut executor = Executor::with_pose(orginal_pose);
+
+        executor.execute("BL");
+
+        let expected_pose = Pose::new(0, 0, 'S');
+        assert_eq!(expected_pose, executor.query());
+    }
+
+    #[test]
+    fn should_return_n_given_status_is_reverse_command_is_r_and_facing_is_e(){
+        let orginal_pose = Pose::new(0, 0, 'E');
+        let mut executor = Executor::with_pose(orginal_pose);
+
+        executor.execute("BR");
+
+        let expected_pose = Pose::new(0, 0, 'N');
+        assert_eq!(expected_pose, executor.query());
+    }
+
+    #[test]
+    fn should_return_y_plus_1_given_command_is_bbm_and_facing_is_e(){
+        let orginal_pose = Pose::new(0, 0, 'E');
+        let mut executor = Executor::with_pose(orginal_pose);
+
+        executor.execute("BBM");
+
+        let expected_pose = Pose::new(0, 1, 'E');
+        assert_eq!(expected_pose, executor.query());
+    }
 }
